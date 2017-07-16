348a2d7b6710736480f5228f0021c73cd2b28fd8
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-07-16 21:19:23 +0300

    comment Entity+Repository added and Blog Entity modified Many to One

--- src/Blogger/BlogBundle/Entity/Blog.php
+++ src/Blogger/BlogBundle/Entity/Blog.php
@@ -3,6 +3,7 @@
 namespace Blogger\BlogBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity(repositoryClass="Blogger\BlogBundle\Entity\Repository\BlogRepository")
@@ -43,6 +44,9 @@ class Blog
      */
     protected $tags;
     
+    /**
+     * @ORM\OneToMany(targetEntity="Comment", mappedBy="blog")
+     */
     protected $comments;
     
     /**
@@ -57,6 +61,8 @@ class Blog
     
     public function __construct()
     {
+        $this->comments = new ArrayCollection();
+        
         $this->setCreated(new \DateTime());
         $this->setUpdated(new \DateTime());
     }
@@ -239,5 +245,39 @@ class Blog
         $this->title = $title;
 
         return $this;
+    }
+
+    /**
+     * Add comment
+     *
+     * @param \Blogger\BlogBundle\Entity\Comment $comment
+     *
+     * @return Blog
+     */
+    public function addComment(\Blogger\BlogBundle\Entity\Comment $comment)
+    {
+        $this->comments[] = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Remove comment
+     *
+     * @param \Blogger\BlogBundle\Entity\Comment $comment
+     */
+    public function removeComment(\Blogger\BlogBundle\Entity\Comment $comment)
+    {
+        $this->comments->removeElement($comment);
+    }
+
+    /**
+     * Get comments
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getComments()
+    {
+        return $this->comments;
     }
 }
