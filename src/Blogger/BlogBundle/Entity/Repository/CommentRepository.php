6443224f46a5a4ef549a0e9c54f3555585e0f88c
Mikhail <mikhail.khvorostianenko@gmail.com>
2017-07-17 22:09:18 +0300

    comments added: Comment repository + Blog controller mod + templates

--- src/Blogger/BlogBundle/Entity/Repository/CommentRepository.php
+++ src/Blogger/BlogBundle/Entity/Repository/CommentRepository.php
@@ -2,6 +2,8 @@
 
 namespace Blogger\BlogBundle\Entity\Repository;
 
+use Doctrine\ORM\EntityRepository;
+
 /**
  * CommentRepository
  *
@@ -10,4 +12,19 @@ namespace Blogger\BlogBundle\Entity\Repository;
  */
 class CommentRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function getCommentsForBlog($blogId, $approved = true)
+    {
+        $qb = $this->createQueryBuilder('c')
+                   ->select('c')
+                   ->where('c.blog =:blog_id')
+                   ->addOrderBy('c.created')
+                   ->setParameter('blog_id', $blogId);
+    
+        if (false === is_null($approved))
+            $qb->andWhere('c.approved = :approved')
+               ->setParameter('approved', $approved);
+    
+        return $qb->getQuery()
+                  ->getResult();
+    }
 }
